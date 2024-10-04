57f9d16fadc3855cfeb62102539a6fc62b16b58e
Muyuan Ma <ma.mu@northeastern.edu>
2024-10-04 17:15:29 -0400

    add

---
+++ src/app/Apps/ToDoApp/ToDoListDisplay.tsx
@@ -0,0 +1,35 @@
+// illustrates 'key' attribute
+import * as React from 'react';
+import {
+    Table, Th, Tbody, Tr,
+} from '@chakra-ui/react';
+
+import { ToDoItem } from './types'
+import { ToDoItemDisplay } from './ToDoItemDisplay'
+
+export function ToDoListDisplay(props: { items: ToDoItem[] , onDelete:(key:number) => void }, sort: boolean) {
+  // if (sort) {
+  //   props.items.sortPriority()
+  // }
+  return (
+    <Table>      
+      <Tbody>
+      <Tr>
+        <Th>Title</Th>
+        <Th>Priority</Th>
+        <Th>Delete</Th>
+      </Tr>
+        {
+          props.items.map((eachItem) => 
+              <ToDoItemDisplay item={eachItem} key={eachItem.key} onDelete={props.onDelete} />)
+        }
+      </Tbody>
+    </Table>
+  )
+}
+
+
+
+export function handleSort(event) {
+  
+}
